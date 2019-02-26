e7bffeb9e24e445eb6723badf29edbf96e030290
arslan <marslanali789@gmail.com>
2019-02-26 21:34:08 +0500

    Problem Set 0 Complete

--- ps0/src/rules/RulesOf6005.java
+++ ps0/src/rules/RulesOf6005.java
@@ -36,9 +36,13 @@ public class RulesOf6005 {
             boolean availableToOthers, boolean writtenAsCourseWork,
             boolean citingYourSource, boolean implementationRequired) {
         
-        // TODO: Fill in this method, then remove the exception
-        
-        throw new RuntimeException("implement me!");
+       boolean allowedToUseCodeInQuestionInAssignment = false;
+       
+       if(writtenByYourself==true && availableToOthers==false && writtenAsCourseWork==true && citingYourSource==true && implementationRequired==true){
+    	   allowedToUseCodeInQuestionInAssignment = true;
+       }
+       
+       return allowedToUseCodeInQuestionInAssignment;
     }
     
     /**
